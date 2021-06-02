35159c18b99e1f52b69c1913cbc8c7b255360fde
Jack <jackcheah2@gmail.com>
2021-06-02 18:06:09 +0800

    Finish hw12

--- hw/hw12/hw12.sql
+++ hw/hw12/hw12.sql
@@ -29,25 +29,42 @@ CREATE TABLE sizes AS
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT name, size FROM dogs, sizes WHERE height > min AND height <= max;
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT child FROM parents, dogs WHERE parent = name ORDER BY height DESC;
 
 -- Filling out this helper table is optional
 CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.child AS small, b.child AS big FROM parents AS a, parents AS b WHERE a.parent = b.parent AND a.child < b.child;
 
 -- Sentences about siblings that are the same size
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT small || " and " || big || " are " || a.size || " siblings" FROM siblings, size_of_dogs AS a, size_of_dogs AS b WHERE small = a.name AND big = b.name AND a.size = b.size;
 
 -- Ways to stack 4 dogs to a height of at least 170, ordered by total height
 CREATE TABLE stacks_helper(dogs, stack_height, last_height);
 
--- Add your INSERT INTOs here
+INSERT INTO stacks_helper 
 
+SELECT 
+	a.dogs || ", " || b.dogs || ", " || c.dogs || ", " || d.dogs, 
+	a.stack_height + b.stack_height + c.stack_height + d.stack_height, 
+	d.stack_height 
+FROM 
+	stacks_helper as a, 
+	stacks_helper as b, 
+	stacks_helper as c, 
+	stacks_helper as d 
+WHERE 
+	a.dogs <> b.dogs AND 
+	c.dogs <> b.dogs AND 
+	c.dogs <> d.dogs AND 
+	a.dogs <> d.dogs AND 
+	b.stack_height > a.stack_height AND 
+	c.stack_height > b.stack_height AND 
+	d.stack_height > c.stack_height;
 
 CREATE TABLE stacks AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT dogs, stack_height FROM stacks_helper WHERE stack_height >= 170 ORDER BY stack_height;
