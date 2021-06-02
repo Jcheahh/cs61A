376deb96b5b3d72d092ebc97213344ac66d6d7ca
Jack <jackcheah2@gmail.com>
2021-06-02 11:57:02 +0800

    Finish hw11

--- hw/hw11/hw11.scm
+++ hw/hw11/hw11.scm
@@ -1,14 +1,26 @@
 (define (find s predicate)
-  'YOUR-CODE-HERE
+  (cond 
+    ((null? s) #f)
+    ((predicate (car s)) (car s))
+    (else (find (cdr-stream s) predicate)))
 )
 
 (define (scale-stream s k)
-  'YOUR-CODE-HERE
+  (cons-stream (* (car s) k)
+      (scale-stream (cdr-stream s) k))
 )
 
 (define (has-cycle s)
-  'YOUR-CODE-HERE
+  (define (has-cycle-helper slow fast)
+    (cond 
+      ((or (null? fast) (null? (cdr-stream fast))) #f)
+      ((or (eq? slow fast) (eq? slow (cdr-stream fast))) #t)
+      (else (has-cycle-helper (cdr-stream slow) (cdr-stream (cdr-stream fast))))))
+  
+  (if (null? s)
+    #f
+    (has-cycle-helper s (cdr-stream s)))
 )
 (define (has-cycle-constant s)
-  'YOUR-CODE-HERE
+  (has-cycle s)
 )
